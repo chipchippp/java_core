4e9c6f7c09eb24ec38557e9eb935ff458a1ac309
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-06-04 22:53:19 +0700

    toán tử điều kiện

---
+++ src/ExportDataAndOperators/ConditionalOperator_15/Main.java
@@ -0,0 +1,10 @@
+package ExportDataAndOperators.ConditionalOperator_15;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int max = (a > b) ? a : b;
+        System.out.println(max);
+    }
+}
