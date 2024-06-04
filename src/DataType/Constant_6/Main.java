2bf44f40e45a234449cba40538435328defed4fa
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-06-04 17:57:12 +0700

    data type and constant

---
+++ src/DataType/Constant_6/Main.java
@@ -0,0 +1,25 @@
+package DataType.Constant_6;
+
+public class Main {
+    public static void main(String[] args) {
+        final int x = 10;
+//        final là một keyword trong Java, nó được sử dụng để khai báo một hằng số.
+//        Một hằng số là một biến mà giá trị của nó không thể thay đổi sau khi đã được gán giá trị.
+        final double y = 10.5;
+        System.out.printf("x: %d\n", x);
+        System.out.printf("y: %.1f\n", y);
+//        x = 100; // Error: cannot assign a value to final variable x
+//        y = 100.5;
+//        System.out.printf("x: %d\n", x);
+
+//        static là gì trong Java?
+//        static là một keyword trong Java, nó được sử dụng để khai báo một biến hoặc phương thức là static.
+//        Biến static là biến mà giá trị của nó được chia sẻ giữa tất cả các đối tượng của lớp.
+//        Phương thức static là phương thức mà có thể được gọi mà không cần tạo một đối tượng của lớp.
+//       Ví dụ:
+        System.out.println("PI: " + Math.PI);
+//        Math.PI là một hằng số static, nó chứa giá trị của số PI (3.141592653589793).
+//        Bạn có thể truy cập nó mà không cần tạo một đối tượng của lớp Math.
+
+    }
+}
---
+++ src/DataType/DataType_5/Main.java
@@ -0,0 +1,23 @@
+package DataType.DataType_5;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Declare a variable named 'number' of type int and assign it a value of 10
+        int number = 10;
+
+        // 2. Declare a variable named 'decimal' of type double and assign it a value of 10.5
+        double decimal = 10.5;
+
+        // 3. Declare a variable named 'character' of type char and assign it a value of 'A'
+        char character = 'A';
+
+        // 4. Declare a variable named 'isTrue' of type boolean and assign it a value of true
+        boolean isTrue = true;
+
+        // 5. Print the values of the variables
+        System.out.println("number: " + number);
+        System.out.println("decimal: " + decimal);
+        System.out.println("character: " + character);
+        System.out.println("isTrue: " + isTrue);
+    }
+}
