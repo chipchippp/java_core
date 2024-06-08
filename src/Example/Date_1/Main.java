53a286839885a80fc520787a87b4304ad919ee6a
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-06-08 18:10:09 +0700

    DateTime

--- src/Example/Date_1/Main.java
+++ src/Example/Date_1/Main.java
@@ -1,7 +1,15 @@
 package Example.Date_1;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        
+        LocalDateTime mObj = LocalDateTime.now();
+        System.out.println("Before formatting: " + mObj);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        String formattedDateTime = mObj.format(formatter);
+        System.out.println("After formatting: " + formattedDateTime);
     }
 }
