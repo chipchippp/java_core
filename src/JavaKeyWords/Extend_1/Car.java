3a6dc923605cd1bc96c0e89c726722acc28ae4cd
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-06-11 22:50:40 +0700

    extend

---
+++ src/JavaKeyWords/Extend_1/Car.java
@@ -0,0 +1,10 @@
+package JavaKeyWords.Extend_1;
+
+public class Car extends Vehicle{
+    private String modelName = "Mustang";
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.honk();
+        System.out.println(myCar.brand + " " + myCar.modelName);
+    }
+}
