5b889b691bfdb1c4747c66b78b03194c64e223d9
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-06-13 17:00:24 +0700

    charAt

---
+++ src/StringMethods/charAt_1/Main.java
@@ -0,0 +1,9 @@
+package StringMethods.charAt_1;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char ch = str.charAt(0);
+        System.out.println(ch); // H
+    }
+}
