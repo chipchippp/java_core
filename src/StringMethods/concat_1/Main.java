4347add9cb55a4645605d0eb50536c9fb30a50bb
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-06-13 18:14:11 +0700

    concat()

---
+++ src/StringMethods/concat_1/Main.java
@@ -0,0 +1,10 @@
+package StringMethods.concat_1;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = str1.concat(str2);
+        System.out.println(str3); // HelloWorld
+    }
+}
