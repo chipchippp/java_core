a238a31c91f9a2830444d16db153fc5d5b07b2d1
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-06-13 17:05:07 +0700

    codePointAt()

--- src/StringMethods/charAt_1/Main.java
+++ src/StringMethods/charAt_1/Main.java
@@ -2,8 +2,14 @@ package StringMethods.charAt_1;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "Hello";
+        String str = "Hello  \uD83C\uDF0D";
         char ch = str.charAt(0);
         System.out.println(ch); // H
+
+//        Phương thức codePointAt() trong Java được sử dụng để lấy mã Unicode của ký tự tại một vị trí xác định trong chuỗi.
+//        Phương thức này có thể hữu ích khi làm việc với các ký tự Unicode mà có thể
+//        không được biểu diễn dưới dạng một ký tự đơn lẻ (như các ký tự ngoài Basic Multilingual Plane của Unicode).
+        int result = str.codePointAt(0);
+        System.out.println(result);
     }
 }
